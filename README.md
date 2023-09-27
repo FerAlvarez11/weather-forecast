b0fb0091cea819c870c85b73d73c3b4267ee78b8
FerAlvarez11 <99476885+FerAlvarez11@users.noreply.github.com>
2023-09-27 20:31:21 +0200

    First commit

--- README.md
+++ README.md
@@ -1,70 +1,20 @@
-# Getting Started with Create React App
+# Steps taken
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+I used the OpenWeatherMap API and added a selector with an event listener to get information depending on the selected city. I stored the necessary data in a useState variable and rendered it in the JSX. Then I used Bulma to style it.
 
-## Available Scripts
+# What could be improved
 
-In the project directory, you can run:
+- At the moment, there is only a console.log if there is no connection to the API. It would be better to do it in a more visual way.
+- Add loading.
+- To improve the accuracy with the city location, it would be beneficial to use the geocoder API available in OpenWeatherMap.
+- Add more information and functionalities.
 
-### `npm start`
+# Instructions for installation
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+```
+git clone https://github.com/FerAlvarez11/weather-forecast.git
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+npm install
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+npm run start
+```
--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import "bulma/css/bulma.min.css"
+import { useState } from "react";
+import BackgroundImage from './bg_image.jpg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const cities = ["Madrid", "Toronto", "Singapore"];
+
+    const [weatherInfo, setWeatherInfo] = useState(null);
+
+    const handleCitySlection = (e) => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=d7fd8be3cca23e5dfbe68064cd686397&units=metric`)
+            .then((response) => response.json())
+            .then((data) => {
+                let dataObject = {
+                    name: data.name,
+                    icon: data.weather[0].icon,
+                    description: data.weather[0].description,
+                    temp: Math.round(data.main.temp),
+                    temp_min: Math.round(data.main.temp_min),
+                    temp_max: Math.round(data.main.temp_max)
+                }
+                setWeatherInfo(dataObject);
+            })
+            .catch(() => {
+                console.log("There was an error communicating with the API.");
+            })
+    }
+
+    return (
+        <div className='App'>
+            <div className="hero is-full-height">
+                <div className="hero-body">
+                    <div className="container" style={{ maxWidth: "300px" }}>
+                        <div className="card">
+                            <div className="pt-6">
+                                <div className="control has-icons-left">
+                                    <div className="select is-medium">
+                                        <select onChange={handleCitySlection} defaultValue={"Select City"}>
+                                            <option disabled="disabled">Select City</option>
+                                            {cities.map((city, i) => <option key={i}>{city}</option>)}
+                                        </select>
+                                        <span className="icon is-medium is-left">
+                                            <i className="fas fa-solid fa-globe"></i>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            {weatherInfo === null ?
+                                <div className="p-4">
+                                    <div className="subtitle is-4">Check the current weather in your city.</div>
+                                    <img src={BackgroundImage} alt="Weather" />
+                                </div>
+                                :
+                                <div>
+                                    <div className="card-image">
+                                        <figure>
+                                            <img src={`https://openweathermap.org/img/wn/${weatherInfo.icon}@4x.png`} alt="Weather Icon" style={{ maxWidth: "200px" }} />
+                                        </figure>
+                                    </div>
+                                    <div className="card-content pt-0">
+                                        <div className="content">
+                                            <h1 className="title is-1">{weatherInfo.temp}°</h1>
+                                            <p className="tag is-rounded subtitle is-6">{weatherInfo.description}</p>
+                                            <p className="subtitle is-4">{weatherInfo.name}</p>
+                                        </div>
+                                    </div>
+                                    <footer className="card-footer">
+                                        <p className="card-footer-item">
+                                            <span>
+                                                <strong>Min: {weatherInfo.temp_min}°</strong>
+                                            </span>
+                                        </p>
+                                        <p className="card-footer-item">
+                                            <span>
+                                                <strong>Max: {weatherInfo.temp_max}°</strong>
+                                            </span>
+                                        </p>
+                                    </footer>
+                                </div>
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
